a1c9799316cf0b6469ea1b3c8c1f22536bcd717c
Ilkka Oksanen <iao@iki.fi>
2015-12-06 19:39:23 +0100

    Use immutable state

--- client/reducers/session.js
+++ client/reducers/session.js
@@ -1,27 +1,42 @@
+import Cookies from 'js-cookie';
+import { Map } from 'immutable';
 import {
     COMPLETE_LOGIN_SUCCESS,
     COMPLETE_LOGIN_FAILURE,
+    UPDATE_HITS,
     LOGOUT
 } from '../actions/session';
 
-export default function session(state = {}, action) {
+const initialState = Map({
+    loggedIn: false,
+    loginFailureReason: '',
+    secret: '',
+    hits: ''
+});
+
+export default function session(state = initialState, action) {
     switch (action.type) {
         case COMPLETE_LOGIN_SUCCESS:
-            return {
+            Cookies.set('secret', action.secret, { expires: 30 }); // days
+            return state.merge({
                 loggedIn: true,
-                loginFailureReason: '',
                 secret: action.secret
-            };
+            });
         case COMPLETE_LOGIN_FAILURE:
-            return {
+            Cookies.remove('secret');
+            return state.merge({
                 loggedIn: false,
                 loginFailureReason: action.reason
-            };
+            });
+        case UPDATE_HITS:
+            return state.merge({
+                hits: action.hits
+            });
         case LOGOUT:
-            return {
+            return state.merge({
                 loggedIn: false,
                 loginFailureReason: ''
-            };
+            });
         default:
             return state;
     }
